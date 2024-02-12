95e1eb796f7564cfc402bd05edbd9d3579166a33
Deepak911741 <dk367748@gmail.com>
2024-02-12 19:25:15 +0530

    is and slicing

---
+++ 01_basic/is.py
@@ -0,0 +1,18 @@
+n = [1, 2, 3]
+m = n
+print(m)
+
+print(n)
+
+m == n
+
+m is n
+
+n = [1, 2, 3]
+m == n
+
+
+m = [1, 2, 3]
+m == n
+
+m is n
---
+++ 01_basic/slicing.py
@@ -0,0 +1,12 @@
+h1 = [1, 2, 3]
+h2 = h1[:]
+
+print(h1)
+print(h2)
+
+h1[0] = 50
+
+print(h1)
+print(h2)
+
+
