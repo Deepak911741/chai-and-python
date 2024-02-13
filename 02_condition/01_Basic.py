d5f0af1ddefd6abfdbaecdf08f48972af8f47ca3
Deepak911741 <dk367748@gmail.com>
2024-02-13 22:17:08 +0530

    condition

---
+++ 02_condition/01_Basic.py
@@ -0,0 +1,13 @@
+# age = int(input("Enter Your Age: "))
+#     # print("Your age is :", age)
+
+
+age = int(input("Enter your age : "))
+
+if age < 13:
+    print("Child")
+elif age < 19:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else("Senior")
