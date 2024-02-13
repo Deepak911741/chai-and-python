017bb20c64ccf960ca9bd744792ec570ef4d8e48
Deepak911741 <dk367748@gmail.com>
2024-02-13 21:28:50 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,7 @@
+tea_types = ("Black", "Green", "Oolong")
+print(tea_types)
+
+print(tea_types[1])
+print(tea_types[1:])
+
+# tea_types[0] = "Lemon" -> esa ha nhi kar sakte hai kyu ki tuple mutable hota hai o change nhi hota hai
