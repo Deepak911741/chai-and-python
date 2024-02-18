921c4b178e05e059a9f0cc8a01873bc703f752a9
Deepak911741 <dk367748@gmail.com>
2024-02-18 22:49:11 +0530

    scope

--- 06_Scopes/01_scope.py
+++ 06_Scopes/01_scope.py
@@ -1,4 +1,17 @@
 username = "Deepak kumar"
 
-def test(): #->esko pythone ne scope kahte hai dusre languge me ese {} es tarh se likte hai
-    pass
+def func(): #->esko pythone ne scope kahte hai dusre languge me ese {} es tarh se likte hai
+    username = "Deepak"
+    print(username)
+    
+print(username)
+func()
+
+
+x = 99
+def func2(y):
+    z = x + y
+    return z
+
+result = func2(1)
+print(result)
